ed2a2ec6555f51ec0093a321a4d489d8f3763d50
joseph patrick soloman <joeboy.patrick@gmail.com>
2025-01-28 17:13:43 +0530

    iam in

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ function sendmail() {
         message : document.getElementById("message").value,
     };
 
-    const serviceID = "service_y83h4ma"
-    const templateID = "template_ppm9m8d"
+    const serviceID = "service_y83h4ma";
+    const templateID = "template_ppm9m8d";
 
     emailjs
       .send(serviceID,templateID,parms)
       .then((res) => {
-        document.getElementById("name").value,
-        document.getElementById("email").value,
-        document.getElementById("subject").value,
-        document.getElementById("message").value,
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("subject").value = "";
+        document.getElementById("message").value = "";
         console.log(res);
         alert("your message is sent succesfully");
       })
