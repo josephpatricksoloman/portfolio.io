288df1dbcd2f7ccca8e62322558e8a4369f51258
joseph patrick soloman <joeboy.patrick@gmail.com>
2025-01-28 16:53:25 +0530

    added the script

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 function sendmail() {
-    let parms = {
+    var parms = {
         name : document.getElementById("name").value,
         email : document.getElementById("email").value,
         subject : document.getElementById("subject").value,
-        message : document.getElementById("message").value
-    }
-    emailjs.send('', '', parms).then(alert("Email sent successfully!"));
+        message : document.getElementById("message").value,
+    };
+
+    const serviceID = "service_y83h4ma"
+    const templateID = ""
+
+    emailjs
+      .send(serviceID,templateID,parms)
+      .then((res) => {
+        document.getElementById("name").value,
+        document.getElementById("email").value,
+        document.getElementById("subject").value,
+        document.getElementById("message").value,
+        console.log(res);
+        alert("your message is sent succesfully");
+      })
+      .catch((err) => console.log(err));
 
 }
